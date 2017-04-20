88c66363b3b7b4e39c62298243312439188f66e6
Etra <sebastian.aedo29@gmail.com>
2017-04-20 20:31:27 -0300

    distribution with series with more than one season

--- get_data.py
+++ get_data.py
@@ -20,7 +20,6 @@ def get_chapters_rates(show):
     pos, id_title, title = show
     url_rating = "http://www.imdb.com/title/{id}/epdate".format(id=id_title)
     text_page = get(url_rating).text
-    # print(text_page)
     tables_regex = r"""<td align="right" bgcolor="#eeeeee">(?P<episode>[\d.]+).*?</td>[\n ]*?<td><a href="/title/.*?/">(?P<name_episode>[\w\W\d]+?)</a></td>[\n ]*?<td align="right">(?P<score>[\d.]+)</td>"""
     tables_regex = re.compile(tables_regex)
 
@@ -29,11 +28,25 @@ def get_chapters_rates(show):
     chapters = [(unescape(episode), unescape(title), unescape(score)) for episode, title, score in chapters]
     return chapters
 
+def get_number_seasons(chapters_list):
+    max = 0
+    for n, name, score in chapters_list:
+        season, chapter = n.strip().split(".")
+        season = int(season)
+        if season > max:
+            max = season
+
+    return max
+
+
 def generate_dataset(list_shows, name):
     with open(name, "w") as dataset:
         dataset.write("show;chapter;name_chapter;score\n")
         for show in list_shows:
-            for chapter, name_chapter, score in get_chapters_rates(show):
+
+            chapters = get_chapters_rates(show)
+
+            for chapter, name_chapter, score in chapters:
 
                 metadata = {
                     "show": show[2],
@@ -45,5 +58,23 @@ def generate_dataset(list_shows, name):
                 dataset.write("{show};{chapter};{name_chapter};{score}\n".format(**metadata))
 
 if __name__ == "__main__":
-    all_series = get_top_shows(7)
-    generate_dataset(all_series, "data3.csv")
+    all_series = get_top_shows(25)
+
+    # we filter the shows with only one season
+    top_series = []
+    TOTAL_SERIES = 5
+
+    i = 0
+    while len(top_series) < TOTAL_SERIES and i < len(all_series):
+
+        chapters = get_chapters_rates(all_series[i])
+        if get_number_seasons(chapters) > 1:
+            print("Se agrego ", all_series[i][2])
+            top_series.append(all_series[i])
+
+        i += 1
+
+    generate_dataset(top_series, "more_seasons.csv")
+    
+
+
